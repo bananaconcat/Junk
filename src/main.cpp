53c2f2aff32dec0e66f1e003144de37f625255fc
cassidy <161962282+fc8k@users.noreply.github.com>
2025-04-20 21:29:40 +0100

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "ferry.hpp"
+
+int main(int argc, char *argv[])
+{
+	std::string sourceCode;
+	std::ifstream sourceFile;
+	sourceFile.open(argv[1]);
+
+	if (sourceFile.is_open())
+	{
+		std::string line;
+		
+		while (getline(sourceFile, line))
+		{
+			sourceCode += line + "\n";
+		}
+	}
+
+	std::cout << sourceCode;
+
+	sourceFile.close();
+
+	auto tokens = tokenize(sourceCode, {"(", ")", "[", "]", "<", ">", "{", "}", ";", "::", ":"}, {"int", "string", "return"});
+
+	for (auto i: tokens)
+		std::cout << i.tokenType << " " << i.literal << "\n";
+
+	return 1;
+}
